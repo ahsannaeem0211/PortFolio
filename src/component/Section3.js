138ada2d7fb1ba0a0fc35fa2bbea7e9728c207ad
Muhammad Ahsan <124510618+ahsannaeem0211@users.noreply.github.com>
2023-06-23 23:14:00 +0500

    first commit

---
+++ src/component/Section3.js
@@ -0,0 +1,44 @@
+import React from 'react'
+// import './Section3.css';
+import './Style.css';
+
+function Section3() {
+  return (
+    <div>
+           <section className="clients section-padding">
+            <div className="container">
+                <div className="row align-items-center">
+
+                    <div className="col-lg-12 col-12">
+                        <h3 className="text-center mb-5">Companies I've had worked</h3>
+                    </div>
+
+                    <div className="col-lg-2 col-4 ms-auto clients-item-height">
+                        <img src="https://templatemo.com/templates/templatemo_578_first_portfolio/images/clients/cachet.svg" className="clients-image img-fluid" alt="abs"/>
+                    </div>
+
+                    <div className="col-lg-2 col-4 clients-item-height">
+                        <img src="https://templatemo.com/templates/templatemo_578_first_portfolio/images/clients/guitar-center.svg" className="clients-image img-fluid" alt="abs"/>
+                    </div>
+
+                    <div className="col-lg-2 col-4 clients-item-height">
+                        <img src="https://templatemo.com/templates/templatemo_578_first_portfolio/images/clients/tokico.svg" className="clients-image img-fluid" alt="abs"/>
+                    </div>
+
+                    <div className="col-lg-2 col-4 clients-item-height">
+                        <img src="https://templatemo.com/templates/templatemo_578_first_portfolio/images/clients/shopify.svg" className="clients-image img-fluid" alt="abs"/>
+                    </div>
+
+                    <div className="col-lg-2 col-4 me-auto clients-item-height">
+                        <img src="https://templatemo.com/templates/templatemo_578_first_portfolio/images/clients/profil-rejser.svg" className="clients-image img-fluid" alt="abs"/>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+      
+    </div>
+  )
+}
+
+export default Section3
