bc85e0f40a36a7e8a8e1d09b89aa6ccb13e0a499
Muhammad Ahsan <124510618+ahsannaeem0211@users.noreply.github.com>
2023-07-02 12:00:26 +0500

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,64 +1,84 @@
-import React from 'react'
-import './Navbar.css';
-// import './Style.css';
+import React, { useState } from 'react'
+import './Style.css';
 
 function Navbar() {
-  return (
-    <div>
-      <div id="sticky-wrapper" className=" is-sticky">
-            <nav className="navbar navbar-expand-lg">
-                <div className="container">
+    const sty = {
+        "css": {
+            "position": "fixed",
+            "top": "0px"
+        }
 
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+    }
+    const [oldsticky, newsticky] = useState("sticky-wrapper");
+    window.onscroll = () => {
+        let top = window.scrollY;
+        let temp;
+        if (top > 50) {
+            temp = "sticky-wrapper is-sticky";
+        }
+        else {
+            temp = "sticky-wrapper";
+        }
+        return newsticky(temp);
+    }
+    return (
+        <div>
+            <div id="sticky-wrapper" className={oldsticky} style={{ height: "93px" }}>
+                <nav className="navbar navbar-expand-lg" style={sty.css}>
+                    <div className="container">
 
-                    <a href="index.html" className="navbar-brand mx-auto mx-lg-0">First</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
 
-                    <div className="d-flex align-items-center d-lg-none">
-                        <i className="navbar-icon bi-telephone-plus me-3"></i>
-                        <a className="custom-btn btn" href="#section_5">
-                            120-240-9600
-                        </a>
-                    </div>
+                        <a href="index.html" className="navbar-brand mx-auto mx-lg-0">First</a>
 
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav ms-lg-5">
-                            <li className="nav-item">
-                                <a className="nav-link click-scroll active" href="#section_1">Home</a>
-                            </li>
-
-                            <li className="nav-item">
-                                <a className="nav-link click-scroll inactive" href="#section_2">About</a>
-                            </li>
-
-                            <li className="nav-item">
-                                <a className="nav-link click-scroll inactive" href="#section_3">Services</a>
-                            </li>
-
-                            <li className="nav-item">
-                                <a className="nav-link click-scroll inactive" href="#section_4">Projects</a>
-                            </li>
-
-                            <li className="nav-item">
-                                <a className="nav-link click-scroll inactive" href="#section_5">Contact</a>
-                            </li>
-                        </ul>
-
-                        <div className="d-lg-flex align-items-center d-none ms-auto">
+                        <div className="d-flex align-items-center d-lg-none">
                             <i className="navbar-icon bi-telephone-plus me-3"></i>
                             <a className="custom-btn btn" href="#section_5">
                                 120-240-9600
                             </a>
                         </div>
-                    </div>
 
-                </div>
-            </nav>
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav ms-lg-5">
+                                <li className="nav-item">
+                                    <a className="nav-link click-scroll active" href="#section_1">Home</a>
+                                </li>
+
+                                <li className="nav-item">
+                                    <a className="nav-link click-scroll inactive" href="#section_2">About</a>
+                                </li>
+
+                                <li className="nav-item">
+                                    <a className="nav-link click-scroll inactive" href="#section_3">Services</a>
+                                </li>
+
+                                <li className="nav-item">
+                                    <a className="nav-link click-scroll inactive" href="#section_4">Projects</a>
+                                </li>
+
+                                <li className="nav-item">
+                                    <a className="nav-link click-scroll inactive" href="#section_5">Contact</a>
+                                </li>
+                            </ul>
+
+                            <div className="d-lg-flex align-items-center d-none ms-auto">
+                                <i className="navbar-icon bi-telephone-plus me-3"></i>
+                                <a className="custom-btn btn" href="#section_5">
+                                    120-240-9600
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
+                </nav>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
+
+
+
